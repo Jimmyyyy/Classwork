3cf588530dfd15d9a8e2d76fa31ae4064fff5a04
Jimmy <Jimmyyyy@users.noreply.github.com>
2016-09-14 13:41:42 -0400

    Made Hello World

--- Period 8 Java/src/introduction/ProceduralExample.java
+++ Period 8 Java/src/introduction/ProceduralExample.java
@@ -2,6 +2,9 @@ package introduction;
 
 public class ProceduralExample {
 	public static void main(String[] args) {
-		
+		System.out.println("Hello World!");
+		// System is a class
+		// out is a field
+		// println is a method of that field
 	}
 }
