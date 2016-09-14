d1d9c67586faeacff54bc0ad5eff263a3d4e9599
Jimmy <Jimmyyyy@users.noreply.github.com>
2016-09-14 14:00:30 -0400

    Update CommitExample.java

--- Period 8 Java/src/introduction/CommitExample.java
+++ Period 8 Java/src/introduction/CommitExample.java
@@ -5,5 +5,13 @@ public class CommitExample {
 		int one = 10;
 		int two = 20;
 		System.out.println(one + two);
+		
+		if (one < 30) {
+			System.out.println("Lmao")
+		}
+		
+		else {
+			System.out.println("faksdjhdj")
+		}
 	}
 }
