cf2b9e069db3eaaa0e353bb37a04a548bd840e6a
Student8 <Student8@10.8.33.151>
2016-09-19 14:00:17 -0400

    fg

---
+++ Period 8 Java/src/introduction/DataTypes.java
@@ -0,0 +1,11 @@
+package introduction;
+
+public class DataTypes {
+	public static void main(String[] args) {
+		double a = 5.0/2;
+		System.out.println((double) 5/2);
+		System.out.println(a);
+	}
+
+
+}
